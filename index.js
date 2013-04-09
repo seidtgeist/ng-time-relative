bbbc03fd419e36e8d74e4892311473edbe3558ad
Stephan Seidt <evilhackerdude@gmail.com>
2013-04-09 12:39:23 +0200

    Watch datetime

--- index.js
+++ index.js
@@ -15,39 +15,48 @@ if (angular) {
 function directive($timeout, moment) {
   return {
     restrict: 'AC',
+    scope: {
+      datetime: '@'
+    },
     link: function(scope, element, attrs) {
-      var date = moment(attrs.datetime || element.text());
-      var to = function() { return moment(attrs.to); };
-      var withoutSuffix = 'withoutSuffix' in attrs;
       var timeout;
 
-      if (!attrs.title)
-        element.attr('title', date.format());
-
-      function updateTime() {
-        element.text(date.from(to(), withoutSuffix));
-      }
-
-      function updateLater() {
-        updateTime();
-        timeout = $timeout(function() {
-          updateLater();
-        }, nextUpdateIn());
-      }
-
-      function nextUpdateIn() {
-        var delta = moment().diff(date);
-        if (delta < 45e3) return 45e3 - delta;
-        if (delta < 90e3) return 90e3 - delta;
-        if (delta < 45 * 60e3) return 60e3 - (delta + 30e3) % 60e3;
-        return 3660e3 - delta % 3600e3;
-      }
-
-      element.bind('destroy', function() {
+      scope.$watch('datetime', function(dateString) {
         $timeout.cancel(timeout);
-      });
 
-      updateLater();
+        var date = moment(dateString);
+        if (!date) return;
+        var to = function() { return moment(attrs.to); };
+        var withoutSuffix = 'withoutSuffix' in attrs;
+
+        if (!attrs.title)
+          element.attr('title', date.format());
+
+        function updateTime() {
+          element.text(date.from(to(), withoutSuffix));
+        }
+
+        function updateLater() {
+          updateTime();
+          timeout = $timeout(function() {
+            updateLater();
+          }, nextUpdateIn());
+        }
+
+        function nextUpdateIn() {
+          var delta = moment().diff(date);
+          if (delta < 45e3) return 45e3 - delta;
+          if (delta < 90e3) return 90e3 - delta;
+          if (delta < 45 * 60e3) return 60e3 - (delta + 30e3) % 60e3;
+          return 3660e3 - delta % 3600e3;
+        }
+
+        element.bind('destroy', function() {
+          $timeout.cancel(timeout);
+        });
+
+        updateLater();
+      });
     }
   };
 }
